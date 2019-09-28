ebac278237146b9d11fcb041fbdc235928a19fae
mojila <mojila@gitlab.com>
2019-09-28 18:26:48 +0700

    update

--- main.m
+++ main.m
@@ -2,21 +2,21 @@ disp("Tugas 3\n");
 
 length = 20;
 
-function num = generate_20()
+function num = generate()
     num = floor(rand(1,1)*10);
     return;
 end
 
 generated = [];
 
-for i = 1:20
-    generated = [generated generate_20()];
+for i = 1:length
+    generated = [generated generate()];
 end
 
 disp("\nHasil Generate 20 Angka antara 0-9");
 disp(generated);
 
-t = 1:20;
+t = 0:9;
 h = hist(generated, t);
 
 disp("\PDF");
